de62af93df9514f533d31d8defd9c5f7f595d59f
Divya <divya.j.nagaraj@gmail.com>
2019-07-10 11:17:36 -0800

    amend commit

---
+++ dir3/two_sum.py
@@ -0,0 +1,9 @@
+def two_sum(list,target):
+    for i in range(0, len(list)):
+        for j in range(i+1, len(list)):
+            if list[i] + list[j] == target:
+                print(f'[ {i}] , [{j}]')
+
+if __name__ == '__main__':
+    list = [3,5,7,8,9,2,4,1]
+    two_sum(list, 9)
