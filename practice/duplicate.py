c2ebf69348b6fe222205a3ccf1be8310c05d5683
Divya <divya.j.nagaraj@gmail.com>
2019-07-13 15:45:58 -0700

    updated more string related comment

--- practice/duplicate.py
+++ practice/duplicate.py
@@ -1,7 +1,16 @@
 def convert_string_to_list():
     str1 = "geeks are here and lets kill then"
+    #use list function
     num1 = list(str1)
     print(num1)
+    num2 = []
+
+    #actually extract character by characted and use append
+    for ch in str1:
+        num2.append(ch)
+    print("num2", num2)
+
+    #use string list and split fucntion to give space or fill list with agrument suggested
     num = list(str1.split(" "))
     print(num)
 
