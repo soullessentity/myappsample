e9741ab113dd28c618969caccd3d5792a9076eee
Divya <divya.j.nagaraj@gmail.com>
2019-07-13 16:49:19 -0700

    updated string function

--- practice/duplicate.py
+++ practice/duplicate.py
@@ -26,13 +26,24 @@ def convert_list_to_string():
     str1 = " "
     print(str1.join(list1))
 
+from collections import Counter
 def counter_charin_string():
     string1 = "hello i am divya"
     cache ={}
+    j = 0
     for ch in string1:
         cache[ch] = string1.count(ch)
     print(cache)
 
+    cache1 = {}
+    for key in cache.keys():
+        if cache[key] > 1:
+            cache1[key]=cache[key]
+    print(cache1)
+
+    chach2 = {}
+    cache2 = Counter(string1)
+    print(cache2)
 
 if __name__ == '__main__':
     convert_string_to_list()
