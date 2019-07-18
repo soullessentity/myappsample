020d0305d85553b610e4c965617bfc9537339c40
Divya <divya.j.nagaraj@gmail.com>
2019-07-17 20:46:35 -0700

    find directory list

--- prog_list/find_dir_path.py
+++ prog_list/find_dir_path.py
@@ -6,5 +6,15 @@ def print_dir_list(root_dir):
             print(full_path)
             print_dir_list(full_path)
 
+def print_file_list(root_dir):
+    for file in os.listdir(root_dir):
+        full_path = os.path.join(root_dir,file)
+        if os.path.isfile(full_path):
+            print(full_path)
+        elif os.path.isdir(full_path):
+            print_file_list(full_path)
+
+
 if __name__ == '__main__':
-    print_dir_list('../')
+    # print_dir_list('../../Myapp1/.git')
+    print_file_list('../../Myapp1')
