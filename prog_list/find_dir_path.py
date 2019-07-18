90e291dd5364297626099419b9fa0dd4203af196
Divya <divya.j.nagaraj@gmail.com>
2019-07-17 20:07:01 -0700

    find directory list

--- prog_list/find_dir_path.py
+++ prog_list/find_dir_path.py
@@ -4,6 +4,7 @@ def print_dir_list(root_dir):
         full_path = os.path.join(root_dir, path)
         if os.path.isdir(full_path):
             print(full_path)
+            print_dir_list(full_path)
 
 if __name__ == '__main__':
     print_dir_list('../')
