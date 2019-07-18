ce99382729024ea1d1a3ba07decbfb66e7518282
Divya <divya.j.nagaraj@gmail.com>
2019-07-17 19:52:41 -0700

    adding dir

---
+++ prog_list/find_dir_path.py
@@ -0,0 +1,9 @@
+import os
+def print_dir_list(root_dir):
+    for path in os.listdir(root_dir):
+        full_path = os.path.join(root_dir, path)
+        if os.path.isdir(full_path):
+            print(full_path)
+
+if __name__ == '__main__':
+    print_dir_list('../')
