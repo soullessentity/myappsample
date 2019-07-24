b57c540b6394665b9f514f25fb4bf6f5f3106f1d
Divya <divya.j.nagaraj@gmail.com>
2019-07-24 14:29:29 -0700

    new pro list for practice

--- prog_list/prog_practice/prog2_list.py
+++ prog_list/prog_practice/prog2_list.py
@@ -29,7 +29,7 @@ def largest_sum_list(l1):
     large = l1[len(l1)-1] + l1[len(l1)-2]
     print(large)
 
-    
+
 if __name__ == '__main__':
     n = [1, 2, 3, 5, 6, 7, 4, 8, 9, 0, 11, 34, 13, 55, 26, 19, 42]
     n1 = [78, 34, 99, 63, 0, 87, 45, 13, 2]
